a28d94e07b223ed88f7003291e40ae00ae065ee6
aditya-123450 <shivam980101@gmail.com>
2025-03-27 00:01:00 +0530

    string program

---
+++ string5.py
@@ -0,0 +1,9 @@
+str1 = "i am student of bca 4th in collage"
+str2 = "i am student of bca 4th in collage"
+str3 = "i am student of bca 4th in collage"
+str4 = "i am student of bca 4th in collage"
+
+print(str1)
+print(str2)
+print(str3)
+print(str4)
