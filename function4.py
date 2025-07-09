9fa6e118737e7bc95407abe92cb1e7a9f85bd46d
aditya-123450 <shivam980101@gmail.com>
2025-07-09 11:30:46 +0530

    function program

---
+++ function4.py
@@ -0,0 +1,10 @@
+# A simple function to calculate the area of a rectangle
+def calculate_area(length, width):
+  area = length * width
+  return area
+
+# Calling the function with different arguments
+area1 = calculate_area(10, 5)  # Result is 50
+area2 = calculate_area(7, 3)   # Result is 21
+
+print(f"Area 1: {area1}, Area 2: {area2}")
